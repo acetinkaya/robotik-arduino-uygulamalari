34640e5919f5a78dceb1a3a872f0642fb602be08
MSc. Ali ÇETİNKAYA <35449105+acetinkaya@users.noreply.github.com>
2024-04-22 00:21:25 +0300

    Add files via upload

---
+++ 1-birinci-uygulama-led.ino
@@ -0,0 +1,36 @@
+/*
+Bu kodlar açık kaynak paylaşım olarak github "acetinkaya" alanında paylaşımı yapılmıştır. 
+
+BTP222 Robotik dersi içerisinde anlatımı yapılan bu kodlar ve içerikler Öğr. Gör. Ali ÇETİNKAYA tarafından oluşturulmuştur. 
+
+Github alanından; 
+watch - ilgili projenin proje takibi bildirimi, 
+star - yıldız ile beğenme bildirimi ve 
+fork - kendi hesabınıza kopyalama işlemleriyle paylaşımlarıma destek verebilirsiniz. 
+
+Bilgi paylaşıldıkça çoğalır ve gelişir. İyi çalışmalar dilerim.
+
+Öğr. Gör. Ali ÇETİNKAYA İstanbul, 2024
+
+Dersin Kaynak Adresleri:
+https://github.com/acetinkaya/robotik-arduino-uygulamalari
+https://alicetinkaya.site/ders-robotik
+https://avesis.gelisim.edu.tr/alcetinkaya 
+https://scholar.google.com.tr/citations?hl=tr&user=XSEW-NcAAAAJ
+*/
+
+const int ledPin = 13; // LED pini
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT); // LED pinini çıkış olarak ayarlandı
+}
+
+void loop() 
+{
+  digitalWrite(ledPin, HIGH); // LED'i yak
+  delay(500); // 1000 milisaniye (1 saniye) bekle
+
+  digitalWrite(ledPin, LOW); // LED'i söndür
+  delay(500); // 1000 milisaniye (1 saniye) bekle
+}
