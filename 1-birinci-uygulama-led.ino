c35beab24b7ad3fe6825383bd5c20be1aa57fe46
MSc. Ali ÇETİNKAYA <35449105+acetinkaya@users.noreply.github.com>
2024-04-22 00:34:59 +0300

    Update 1-birinci-uygulama-led.ino

--- 1-birinci-uygulama-led.ino
+++ 1-birinci-uygulama-led.ino
@@ -17,6 +17,16 @@ https://github.com/acetinkaya/robotik-arduino-uygulamalari
 https://alicetinkaya.site/ders-robotik
 https://avesis.gelisim.edu.tr/alcetinkaya 
 https://scholar.google.com.tr/citations?hl=tr&user=XSEW-NcAAAAJ
+
+---------------------------------------------------------------------------------------------------------
+Kodun açıklaması: 
+
+Arduino Nano üzerinde bağlı olan 13. ledin yarım saniye aralıklar ile yanıp sönmesini hedef ediniyoruz.
+Bu kapsamda pinMode fonksiyonu ile arduino pinlerini giriş (INPUT) veya çıkış (OUTPUT) yapmaya yarar. 
+digitalWrite kodu ile ilgili pin üzerinde HIGH tanımlaması varsa pin çıkışı aktif-5V, LOW tanımlaması var ise pin çıkışı pasif-0V olarak ayarlanmaktadır. 
+Arduino platformunda atmega programlar iken iki önemli fonksiyon bulunmaktadır. setup() fonksiyonu ile denetleyicinin ayarları yapılır. loop() fonksiyonu ile denetleyicinin program döngüsü çalıştırılmaktadır.
+delay() fonksiyonu ile;  program döngüsünün belirli bir süre boyunca kod üzerinde çalışmasını sağlamaktadır. 
+
 */
 
 const int ledPin = 13; // LED pini
