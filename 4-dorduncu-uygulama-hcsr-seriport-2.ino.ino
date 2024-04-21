d1691bf41d9a38b66449db74047c0a91d1a474c2
MSc. Ali ÇETİNKAYA <35449105+acetinkaya@users.noreply.github.com>
2024-04-22 01:18:55 +0300

    Add files via upload

---
+++ 4-dorduncu-uygulama-hcsr-seriport-2.ino.ino
@@ -0,0 +1,45 @@
+/*
+Bu kodlar açık kaynak paylaşım olarak github "acetinkaya" alanında paylaşımı yapılmıştır. 
+
+BTP222 Robotik dersi içerisinde anlatımı yapılan bu kodlar ve içerikler Öğr. Gör. Ali ÇETİNKAYA tarafından oluşturulmuştur. 
+
+Github alanından; 
+watch - ilgili projenin proje takibi bildirimi, 
+star - yıldız ile beğenme bildirimi ve 
+fork - kendi hesabınıza kopyalama işlemleriyle paylaşımlarıma destek verebilirsiniz. 
+
+Bilgi paylaşıldıkça çoğalır ve gelişir. İyi çalışmalar dilerim.
+
+Öğr. Gör. Ali ÇETİNKAYA İstanbul, 2024
+
+Dersin Kaynak Adresleri:
+https://github.com/acetinkaya/robotik-arduino-uygulamalari
+https://alicetinkaya.site/ders-robotik
+https://avesis.gelisim.edu.tr/alcetinkaya 
+https://scholar.google.com.tr/citations?hl=tr&user=XSEW-NcAAAAJ
+
+Kodun açıklaması: 
+HC-SR04 ultrasonik mesafe sensörünün kütüphaneli okuma işlemidir.
+
+Bu işlem için Tools->Library Manager->HCSR04 ara-> "HCSR04 by Martin Sosic" bilgisini içeren 
+kütüphaneyi install ile kurulumunu yaparız. Kurulum yapıldıktan sonra File-> Examples -> HCSR04-> simple
+çalıştırılarak aşağıdaki kodlar gelmektedir. Aşağıdaki kodlarda;
+
+Arduino Nano üzerinde dijital pin bölümünde 13. ve 12. pinlere bağlı 
+ultrasonik mesafe sensörü ile ölçüm alıp, seri port üzerinden mesafenin basılmasıdır.
+
+*/
+
+#include <HCSR04.h>
+
+UltraSonicDistanceSensor distanceSensor(9, 10);  // Initialize sensor that uses digital pins 13 and 12.
+
+void setup () {
+    Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
+}
+
+void loop () {
+    // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
+    Serial.println(distanceSensor.measureDistanceCm());
+    delay(200);
+}
