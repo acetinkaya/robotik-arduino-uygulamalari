9a9cf43bc0609dc9f0fc15078278c6fbc54c6b46
Lect. Ali Cetinkaya (MSc.) <35449105+acetinkaya@users.noreply.github.com>
2024-04-28 00:19:47 +0300

    Add files via upload

---
+++ 25-l298motorsurucu.ino
@@ -0,0 +1,60 @@
+/*
+Bu kodlar açık kaynak paylaşım olarak github "acetinkaya" alanında paylaşımı yapılmıştır. 
+
+BTP222 Robotik dersi içerisinde anlatımı yapılan bu kodlar ve içerikler Öğr. Gör. Ali ÇETİNKAYA tarafından oluşturulmuştur. 
+
+Github alanından; 
+watch - ilgili projenin proje takibi bildirimi, 
+star - yıldız ile beğenme bildirimi ve 
+fork - kendi hesabınıza kopyalama işlemleriyle paylaşımlarıma destek verebilirsiniz. 
+
+Bilgi paylaşıldıkça çoğalır ve gelişir. İyi çalışmalar dilerim.
+
+Öğr. Gör. Ali ÇETİNKAYA İstanbul, 2024
+
+Dersin Kaynak Adresleri:
+https://github.com/acetinkaya/robotik-arduino-uygulamalari
+https://alicetinkaya.site/ders-robotik
+https://avesis.gelisim.edu.tr/alcetinkaya 
+https://scholar.google.com.tr/citations?hl=tr&user=XSEW-NcAAAAJ
+
+Kodun açıklaması: l298 motor sürücü ile dc motor sürme uygulaması
+
+*/
+
+int DC_Motor_PWM = 3; // Motor hız kontrol pini (PWM)
+int DC_Motor_A = 11;    // Motor yön kontrol pini 11
+int DC_Motor_B = 12;    // Motor yön kontrol pini 12
+
+void setup()
+ {
+  // Pinlerin çıkış olarak ayarlanması
+  pinMode(DC_Motor_PWM, OUTPUT);
+  pinMode(DC_Motor_A, OUTPUT);
+  pinMode(DC_Motor_B, OUTPUT);
+
+  delay(100);
+
+  digitalWrite(DC_Motor_A, LOW);
+  digitalWrite(DC_Motor_B, LOW);
+  delay(100);
+}
+
+void loop() 
+{
+  // Motorun ileri yönde hareket etmesi
+  digitalWrite(DC_Motor_A, HIGH);
+  digitalWrite(DC_Motor_B, LOW);
+  analogWrite(DC_Motor_PWM, 255); // PWM sinyali ile hız kontrolü (255 maksimum hız)
+
+  // 5 saniye bekleme
+  delay(5000);
+
+  // Motorun geri yönde hareket etmesi
+  digitalWrite(DC_Motor_A, LOW);
+  digitalWrite(DC_Motor_B, HIGH);
+  analogWrite(DC_Motor_PWM, 255); // PWM sinyali ile hız kontrolü (255 maksimum hız)
+
+  // 5 saniye bekleme
+  delay(5000);
+}
