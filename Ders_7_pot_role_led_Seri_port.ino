768e4afc4e00e45f87739c866cb6e2ba26e15f81
Lect. Ali Cetinkaya (MSc.) <35449105+acetinkaya@users.noreply.github.com>
2025-04-07 16:37:09 +0300

    Add files via upload

--- Ders_7_pot_role_led_Seri_port.ino
+++ Ders_7_pot_role_led_Seri_port.ino
@@ -1,3 +1,31 @@
+/*
+Bu kodlar açık kaynak paylaşım olarak github "acetinkaya" alanında paylaşımı yapılmıştır. 
+
+BTP222 Robotik dersi içerisinde anlatımı yapılan bu kodlar ve içerikler Öğr. Gör. Ali ÇETİNKAYA tarafından oluşturulmuştur. 
+
+Github alanından; 
+watch - ilgili projenin proje takibi bildirimi, 
+star - yıldız ile beğenme bildirimi ve 
+fork - kendi hesabınıza kopyalama işlemleriyle paylaşımlarıma destek verebilirsiniz. 
+
+Bilgi paylaşıldıkça çoğalır ve gelişir. İyi çalışmalar dilerim.
+
+Öğr. Gör. Ali ÇETİNKAYA İstanbul, 2024
+
+Dersin Kaynak Adresleri:
+https://github.com/acetinkaya/robotik-arduino-uygulamalari
+https://alicetinkaya.site/ders-robotik
+https://avesis.gelisim.edu.tr/alcetinkaya 
+https://scholar.google.com.tr/citations?hl=tr&user=XSEW-NcAAAAJ
+
+---------------------------------------------------------------------------------------------------------
+Kodun açıklaması: 
+
+Arduino üzerinde bağlı potansiyometre ile röle durumunu kontrol edilerek ledlerin durumunu
+işlenmektedir. 
+
+*/
+
 const int analogPin = A0;   // Analog giriş (potansiyometre/sensör)
 const int ledPin = 5;       // PWM çıkışı (LED için)
 const int rolePin = 7;      // Dijital çıkış (Röle kontrol)
