22b4fd97efa3e320eb7b363a6c2c4a71ce5e86d0
Lect. Ali Cetinkaya (MSc.) <35449105+acetinkaya@users.noreply.github.com>
2025-04-22 09:47:50 +0300

    Update Ders_13_DHT_MQ2_Seri_port.ino

--- Ders_13_DHT_MQ2_Seri_port.ino
+++ Ders_13_DHT_MQ2_Seri_port.ino
@@ -24,7 +24,7 @@ https://scholar.google.com.tr/citations?hl=tr&user=XSEW-NcAAAAJ
 
 ---
 
-DHT Kütüphanesini Arduino menülerinden "Tools" -> "Manage Libraries..." -> arama menüsünden -> "DHT sensor library by Adafruit" bularak "INSTALL" ile kurulum sağlayın, Kurulum 
+DHT Kütüphanesini Arduino menülerinden "Tools" -> "Manage Libraries..." -> arama menüsünden -> "DHT sensor library by Adafruit" bularak "INSTALL" ile kurulum sağlayın, Kurulum tamamlanmdıktan sonra...
 
 ---
 
